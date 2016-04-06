ffd79c2d1a5e5a34888ae99c33301a7affa21f52
MicSpring <subslg@gmail.com>
2016-04-07 00:52:39 +0530

    Spring Cloud Client2
    Hystrix Configuration for Streaming Metrics

--- src/main/java/com/subha/client2/ClientStarter2.java
+++ src/main/java/com/subha/client2/ClientStarter2.java
@@ -2,14 +2,15 @@ package com.subha.client2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
-@EnableHystrix
+//@EnableHystrix
+@EnableCircuitBreaker
 public class ClientStarter2 {
 	
 	public static void main(String[] args)
